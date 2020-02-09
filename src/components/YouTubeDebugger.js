52805995fd5f81f10233d3e134d1738b08796f13
Kateryna Strough <katerynastrough@hotmail.com>
2020-02-09 13:41:42 +0000

    Automatically backed up by Learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -38,7 +38,7 @@ class YouTubeDebugger extends React.Component {
 
     /*
     Or this can be defined using the Object.assign method:
-    
+
     this.setState({
       settings: Object.assign({}, this.state.settings, {
         video: Object.assign({}, this.state.settings.video, {
@@ -46,7 +46,7 @@ class YouTubeDebugger extends React.Component {
         })
       })
     });
-    
+
     */
   };
 
